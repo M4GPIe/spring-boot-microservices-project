6c395e7038090b36344231e47e652eb45aac7f5f
Manu <manuel.delossantos@dialapplet.com>
2025-10-20 13:55:08 +0200

    feat: repository and mock data

--- inventory-service/src/main/java/com/m4gpie/inventory_service/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/m4gpie/inventory_service/repository/InventoryRepository.java
@@ -1,9 +1,11 @@
 package com.m4gpie.inventory_service.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.m4gpie.inventory_service.models.Inventory;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
-
+    Optional<Inventory> findBySkuCode(String skuCode);
 }
