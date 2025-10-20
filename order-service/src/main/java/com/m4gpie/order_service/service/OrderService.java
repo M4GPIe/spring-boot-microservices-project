4cf98dbd3f314b00d390269afbe3ac9652d0cf34
Manu <manuel.delossantos@dialapplet.com>
2025-10-20 12:49:49 +0200

    feat: order service

---
+++ order-service/src/main/java/com/m4gpie/order_service/service/OrderService.java
@@ -0,0 +1,44 @@
+package com.m4gpie.order_service.service;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import com.m4gpie.order_service.dto.OrderLineItemsDto;
+import com.m4gpie.order_service.dto.OrderRequest;
+import com.m4gpie.order_service.model.Order;
+import com.m4gpie.order_service.model.OrderLineItems;
+import com.m4gpie.order_service.repository.OrderRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapItemsFromDto)
+                .toList();
+
+        order.setOrderLineItemsList(orderLineItems);
+
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems mapItemsFromDto(OrderLineItemsDto orderLineItemsDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(orderLineItemsDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
+
+        return orderLineItems;
+    }
+}
