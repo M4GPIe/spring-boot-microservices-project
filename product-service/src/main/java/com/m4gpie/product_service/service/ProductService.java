05ee9b6cb157f0e3e9553cb79d01194661b46ac7
Manu <manuel.delossantos@dialapplet.com>
2025-10-20 12:47:39 +0200

    feat: product service

---
+++ product-service/src/main/java/com/m4gpie/product_service/service/ProductService.java
@@ -0,0 +1,48 @@
+package com.m4gpie.product_service.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.m4gpie.product_service.dto.ProductRequest;
+import com.m4gpie.product_service.dto.ProductResponse;
+import com.m4gpie.product_service.model.Product;
+import com.m4gpie.product_service.respository.ProductRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public void createProduct(ProductRequest productRequest) {
+
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+
+        productRepository.save(product);
+        log.info("Product {} is saved", product.getId());
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+
+        return products.stream().map(this::mapProductResponse).toList();
+    }
+
+    private ProductResponse mapProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
+    }
+}
